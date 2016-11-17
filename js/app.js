e636f5cd89c07d42b90e4f2c27b69600e7bb9dc9
Tristan Van Maren <tm.vanmaren@gmail.com>
2016-11-17 00:57:05 -0700

    add input checking to bar invalid keys including shift conditions --current bug: error output not toggling ready state

--- js/app.js
+++ js/app.js
@@ -2,36 +2,70 @@
 
 var $screen;
 var readyForMore;
+var shiftKeyDown = false;
 
-$(function() {
+$(function () {
 
-    $screen = $('#screen');
-    readyForMore = false;
+  $screen = $('#screen');
+  readyForMore = false;
 
-    $('#buttons-container').click(handleClick);
-    $('#screen').keyup(handleKeyboard);
+  $('#buttons-container').click(handleClick);
+  $('#screen').keydown(handleKeyDown);
+  $('#screen').keyup(shiftOff);
+
 });
 
 function handleClick(event) {
-    var $button = $(event.target);
-    if ($button.is('span')) {
-        if ($button.attr('class') === 'operator') {
-          //perform operation
-            perform($button.attr('id') || $button.text());
-        } else {
-            pushToScreen($button.text());
-        }
+  var $button = $(event.target);
+  if ($button.is('span')) {
+    if ($button.attr('class') === 'operator') {
+      //perform operation
+      perform($button.attr('id') || $button.text());
+    } else {
+      pushToScreen($button.text());
     }
-    return;
+  }
+  return;
 }
 
-function handleKeyboard(event) {
-  var key=event.which;
+function handleKeyDown(event) {
+  var key = event.which;
   console.log(key);
-  if (key===13) {
-    $('#equals').trigger('click');
+
+  if (key === 16) {
+    shiftKeyDown = true;
+    return true;
   }
-  if (key===27 || 0) {
-    $('#clear').trigger('click');
+
+  if (!isValidKey(key)) {
+    event.preventDefault();
+    return false;
   }
+
+  if (isOperationKey(key)) {
+    switch (key) {
+    case (13):
+      $('#equals').trigger('click');
+      return;
+    case (27):
+      $('#clear').trigger('click');
+      return;
+    case (0): //fix for firefox, where ESC registers as 0
+      $('#clear').trigger('click');
+      return;
+    }
+  }
+  return true;
+}
+
+function shiftOff(event) {
+  var key = event.which;
+  if (key === 16) {
+    shiftKeyDown = false;
+  }
+  return;
+}
+
+function isOperationKey(key) {
+  return ((key===13) || (key===27));
 }
